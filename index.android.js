fda9e608798437a661d0bb9ff65db5c76739e64b
Sami Kukkonen <sami@samikukkonen.fi>
2017-03-24 20:33:11 +0200

    Add camera view to UI

--- index.android.js
+++ index.android.js
@@ -11,21 +11,16 @@ import {
   Text,
   View
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class Tarant extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Camera
+          style={styles.camera}
+          aspect={Camera.constants.Aspect.fill}
+        />
       </View>
     );
   }
@@ -34,20 +29,14 @@ export default class Tarant extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
+    backgroundColor: 'red',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  camera: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  }
 });
 
 AppRegistry.registerComponent('Tarant', () => Tarant);
